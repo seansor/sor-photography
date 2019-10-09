e82116f7f2f7295b896f74830d2da08bbfefc389
Ubuntu <ubuntu@ip-172-31-39-115.ec2.internal>
2019-10-09 12:06:35 +0000

    initial commit, sets up django project, and sets up basic user registration and login

---
+++ README.md
@@ -0,0 +1,4 @@
+# SOR Photography
+
+## A website to purchase photos taken by SOR Photography or to request 
+## commissions
---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url, include
+from accounts.views import index, logout, login, register, user_profile
+from accounts import urls_reset
+
+
+urlpatterns = [
+    url(r'^logout/$', logout, name="logout"),
+    url(r'^login/$', login, name="login"),
+    url(r'^register/$', register, name="register"),
+    url(r'^profile/$', user_profile, name="profile"),
+    url(r'^password_reset/', include(urls_reset))
+]
