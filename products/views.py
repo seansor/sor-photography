57d26a80f00eaaa488f868e7ed4c2d6c80c95b7f
Ubuntu <ubuntu@ip-172-31-39-115.ec2.internal>
2019-10-14 18:45:43 +0000

    Adds gallery view functionality to collection thumbnails and provides links to purchase items

--- products/views.py
+++ products/views.py
@@ -10,27 +10,35 @@ logger = logging.getLogger(__name__)
 # Create your views here.
 
 def all_products(request):
-    #current_series = Product.objects.get(collection="Pubs of Dublin")
     products = Product.objects.all()
     collections = Collection.objects.all()
+    first_product_ids = []
     collection_ids = []
     collection_images = []
+    collection_image_names = []
+    collection_names = []
     for collection in collections:
-        product = Product.objects.filter(collection_id=collection.id).first()
-        collection_image = product.image
-        collection_images.append(collection_image)
-        collection_id = product.collection_id
+        first_product = Product.objects.filter(collection_id=collection.id).first()
+        first_product_id = first_product.id
+        first_product_ids.append(first_product_id)
+        collection_id = first_product.collection_id
         collection_ids.append(collection_id)
-    logger.debug(collection_images)
-    logger.debug(collection_ids)
-    collections = Collection.objects.values()
+        collection_image = first_product.image
+        collection_images.append(collection_image)
+        collection_image_name = first_product.name + ', ' + first_product.location
+        collection_image_names.append(collection_image_name)
+        collection_name = collection.name
+        collection_names.append(collection_name)
+    
+    collection_details = zip(first_product_ids, collection_ids, collection_images, collection_image_names, collection_names)
     current_series = Collection.objects.get(current_series=True)
-    return render(request, 'products.html', {'products': products,  'current_series': current_series, 'collections': collections })
+    return render(request, 'products.html', {'products': products,  'current_series': current_series,
+                'collections': collections, 'collection_details':  collection_details})
 
 
 def product(request, id):
     product = Product.objects.get(id=id)
-    product_variants = list(ProductVariant.objects.values())
-    logger.info(product_variants)
+    product_variants = ProductVariant.objects.filter(product_id=id)
+    #product_variants = list(ProductVariant.objects.values(product_id=id))
     #price_of_each_size = zip(product_variants.size, product_variants.price)
     return render(request, 'product.html', {'product': product, 'product_variants': product_variants})
